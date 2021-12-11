978b546bdf6780fbdd0cfaf9239a7a5777ba548c
ADR-Clizy <adr.clizy@outlook.fr>
2021-12-11 20:41:01 +0100

    TR28 - Fixing Profile bugs

---
+++ AndromedeAlpha/DatabaseConnection/RestorerModels/RestorerProfile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConnection.RestorerModels
+{
+    public class RestorerProfile
+    {
+        [RegularExpression(@"^\d+$",
+         ErrorMessage = "Seuls les chiffres sont autorisés")]
+        public string RestaurantSiretNumber { get; set; }
+
+
+        [Required(ErrorMessage = "Champ requis")]
+        [RegularExpression(@"^[a-zA-Z0-9éèçëïêîèìœ\s]+$",
+         ErrorMessage = "Caractères spéciaux interdits")]
+        [MaxLength(50, ErrorMessage = "Champ trop long")]
+        public string RestaurantName { get; set; }
+
+
+        [RegularExpression(@"^[a-zA-Zéèçëïêîèìœ\s]+$",
+         ErrorMessage = "Chiffres / Caractères spéciaux interdits")]
+        [MaxLength(40, ErrorMessage = "Champ trop long")]
+        public string FirstName { get; set; }
+
+
+        [RegularExpression(@"^[a-zA-Zéèçëïêîèìœ\s]+$",
+         ErrorMessage = "Chiffres / Caractères spéciaux interdits")]
+        [MaxLength(40, ErrorMessage = "Champ trop long")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Champ requis")]
+        [EmailAddress(ErrorMessage = "Format incorrect")]
+        public string EmailAddress { get; set; }
+
+
+        [Required(ErrorMessage = "Champ requis")]
+        [Phone(ErrorMessage = "Format incorrect")]
+        public string PhoneNumber { get; set; }
+    }
+}
